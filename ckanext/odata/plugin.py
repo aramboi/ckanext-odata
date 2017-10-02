12e8372b70d0c1cbe5e80a625e58f79c69b776ff
Jay Guo <jayguo@jays-mbp.lan>
2017-10-02 16:26:51 -0400

    Changes specific to Boston

--- ckanext/odata/plugin.py
+++ ckanext/odata/plugin.py
@@ -1,4 +1,5 @@
 import ckan.plugins as p
+import ckan.plugins.toolkit as toolkit
 
 import ckanext.odata.actions as action
 
@@ -6,6 +7,13 @@ import ckanext.odata.actions as action
 def link(resource_id):
     return '%s%s' % (action.base_url(), resource_id)
 
+def resource_format(resource_id):
+    result = toolkit.get_action('resource_show')({}, {'id': resource_id})
+    res_format = result.get('format','').lower()
+    if res_format == 'csv':
+        return True
+    else:
+        return False
 
 class ODataPlugin(p.SingletonPlugin):
 
@@ -37,4 +45,5 @@ class ODataPlugin(p.SingletonPlugin):
     def get_helpers(self):
         return {
             'ckanext_odata_link': link,
+            'ckanext_odata_res_format': resource_format,
         }
